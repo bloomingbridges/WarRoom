9cf4565c413985756d9cd86e0cafd0df7ce04998
bloomingbridges <brueckner.florian@googlemail.com>
2013-06-12 19:26:27 +0100

    Working nodemailer system.

--- Node/main.js
+++ Node/main.js
@@ -1,5 +1,34 @@
-var net = require('net');
+
+// Modules /////////////////////////////////////////////////////////////////////
+
+var net = require('net')
+  , nodemailer = require("nodemailer")
+  , credentials = require("./credentials.js");
+
+
+// Globals /////////////////////////////////////////////////////////////////////
+
+var port = process.env.PORT || 6666;
 var mySocket;
+
+var transport = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
+    auth: {
+        user: credentials.user,
+        pass: credentials.pwd
+    }
+});
+
+var testEmail = {
+	from: "▇▇▇▇▇▇▇▇▇▇▇▇ <missilenotificationsystem@gmail.com>",
+	to: "Evildoer <j@lostwith.me>",
+	subject: "▇▇▇▇▇ Test",
+	text: "Yo James",
+	html: "<h1>Hey Jimbo</h1><p>You just killed a fair amount of people. THX.</p>"
+};
+
+
+// Flash ///////////////////////////////////////////////////////////////////////
 
 var server = net.createServer(function(socket) {
   mySocket = socket;
@@ -13,8 +42,29 @@ function onConnect() {
 
 function onData(details) {
 	console.log(details);
+	sendEmail(null, 1000);
 	// TODO Make sure passphrase is correct ////////////////////////////////////
 	// TODO Process incoming data //////////////////////////////////////////////
 }
 
-server.listen(8080, '127.0.0.1');
+
+// Mail ////////////////////////////////////////////////////////////////////////
+
+function sendEmail(recipient, impact) {
+	transport.sendMail(testEmail, function(error, response) {
+		if (error) {
+			console.log(error);
+		}
+		else {
+			console.log("Mail has been sent successfully :>");
+			console.log(response);
+		}
+	});
+}
+
+
+// GO //////////////////////////////////////////////////////////////////////////
+
+server.listen(port, '127.0.0.1');
+console.log("TCP SERVER LISTENING ON PORT " + port + " YO!");
+
