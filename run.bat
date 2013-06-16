bd4c71f9020416de1fa3463ade9346f1a946293d
bloomingbridges <brueckner.florian@googlemail.com>
2013-06-16 18:51:08 +0200

    Final update to run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 SET DRIVER=C:\Program Files (x86)\Sensory Software\Switch Driver
-START /B "%DRIVER%\Switch Driver.exe"
+START /B "%DRIVER%" "%DRIVER%\Switch Driver.exe"
 START Flash/WarRoom.exe
 NODE Node/main.js
